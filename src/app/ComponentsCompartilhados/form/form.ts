8afe4f4b585d2fb85641eed4505387f14e1b6781
CaioRen4to <caio.renato011@gmail.com>
2025-11-16 15:03:04 -0300

    Criação dos componentes compartilhados e a implementação no cadastro!

--- src/app/ComponentsCompartilhados/form/form.ts
+++ src/app/ComponentsCompartilhados/form/form.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
+  @Input() title: string = '';
+  @Input() fields: Array<{ label: string; type: string; name: string; placeholder: string }> = [];
 
+  @Output() dataChanged = new EventEmitter<any>();  
+
+  formData: Record<string, any> = {};
+
+  ngOnInit() {
+    this.fields.forEach(field => {
+      this.formData[field.name] = '';
+    });
+  }
+
+  onFieldChange() {
+    this.dataChanged.emit(this.formData);
+  }
 }
+
--- src/app/pages/validacao/cadastro/cadastro.ts
+++ src/app/pages/validacao/cadastro/cadastro.ts
@@ -1,11 +1,73 @@
 import { Component } from '@angular/core';
+import { Button } from "../../../ComponentsCompartilhados/button/button";
+import { Form } from "../../../ComponentsCompartilhados/form/form";
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [Button, Form],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
+  message: string = '';
+  formData: any = {};
 
+  cadastroFields = [
+    { 
+      label: 'Nome', 
+      type: 'text', 
+      name: 'nome', 
+      placeholder: 'Digite seu nome' 
+    },
+    { 
+      label: 'Email', 
+      type: 'email', 
+      name: 'email', 
+      placeholder: 'Digite seu email' 
+    },
+    { 
+      label: 'Senha', 
+      type: 'password', 
+      name: 'senha', 
+      placeholder: 'Digite sua senha' 
+    },
+  ];
+
+  constructor(private router: Router) {}
+
+  updateFormData(data: any) {
+    this.formData = data;
+  }
+
+  cadastrar() {
+    if (!this.formData.nome || !this.formData.email || !this.formData.senha) {
+      this.message = 'Por favor, preencha todos os campos.';
+      return;
+    }
+
+    const newUser = {
+      nome: this.formData.nome,
+      email: this.formData.email,
+      senha: this.formData.senha,
+    };
+
+    const userJson = localStorage.getItem('users');
+    let users = userJson ? JSON.parse(userJson) : [];
+
+    const userExists = users.some((user: any) => user.email === newUser.email);
+    if (userExists) {
+      this.message = 'Este email já está cadastrado. Tente Fazer o Login';
+      return;
+    }
+
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
+
+    this.message = 'Cadastro realizado com sucesso! Redirecionando para o login...';
+
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 2000);
+  }
 }
