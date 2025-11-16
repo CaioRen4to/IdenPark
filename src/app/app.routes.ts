f3bef67c4966ad2a2b8918040e3eaeabf0769a0e
CaioRen4to <caio.renato011@gmail.com>
2025-11-16 00:39:02 -0300

    Criação dos Components/Compartilhados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/validacao/login/login';
+import { Cadastro } from './pages/validacao/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'cadastro',
+        component: Cadastro
+    }
+];
