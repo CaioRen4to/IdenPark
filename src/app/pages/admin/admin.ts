4d69c0e444cc8d5593e47b3de887906cc6c4ab5c
CaioRen4to <caio.renato011@gmail.com>
2025-11-20 14:58:42 -0300

    Alteração do estilo dos componentes

--- src/app/pages/admin/admin.ts
+++ src/app/pages/admin/admin.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../ComponentsCompartilhados/sidebar/sidebar";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
